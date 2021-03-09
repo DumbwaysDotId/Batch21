071cce3b231c863212529f0e67460b4fd18b587d
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-03-09 10:33:06 +0700

    Handling Event

--- src/components/content.js
+++ src/components/content.js
@@ -8,12 +8,18 @@ function Content(){
         return 19+2;
     }
 
+    function cobaAlert(){
+        alert("Conver Regular Function with Functional Component");
+    }
+
     const title = "Bootcamp Dumbways with Functional Components"
 
     return(
         <div className="App-header">
             <p>Welcome to Dumbways.id</p>
             <p>Welcome {title} Batch {getBatch()}</p>
+
+            <button onClick={cobaAlert}>Hit Me</button>
         </div>
     )
 }
@@ -24,12 +30,20 @@ function Content(){
 //         return 20+1;
 //     }
 
+//     cobaAlert(){
+//         alert("Convert Function")
+//     }
+
 //   render(){
+    
 //       const title = "Bootcamp Dumbways";
+
 //     return(
 //       <div className="App-header">
 //         <p>This Is Content</p>
 //         <p>Welcom {title} Batch {this.getBatch()}</p>
+
+//         <button onClick={this.cobaAlert}>Hit Me</button>
 //       </div>
 //     )
 //   }
